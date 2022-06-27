909bad8153ec2a10245b257a8598892144769b69
Mamadou Diagne <genova@dofbi.com>
2022-06-27 12:59:18 +0000

    ✨ feat (astro-i18next): AOD-10 Internationalisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
 
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  site: "https://open-data-africtivistes.netlify.app/",
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    tailwind(),
+    astroI18next({
+      baseLanguage: "fr",
+      i18next: {
+        debug: false,
+        supportedLngs: ["en", "fr"],
+      },
+    }),
+  ],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"],
